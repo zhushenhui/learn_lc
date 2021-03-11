4d23c00a199da0842fd939602dac5e5f5ca9a93a
zhushenhui <908682858@qq.com>
2021-03-11 22:02:18 +0800

    28题完整提交

--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,12 @@
+// 28. 实现 strStr()
 class Solution {
     public static int strStr(String haystack, String needle) {
+        // 如果子串长度为0返回0
         if (needle.length() == 0)
             return 0;
-        for (int i = 0; i < haystack.length() - needle.length(); i++) {
+        // 暴力法，母串从最左侧开始匹配，匹配时母串剩余长度不能比字串短
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            // 首字符匹配则继续
             if (haystack.charAt(i) == needle.charAt(0)) {
                 boolean flag = true;
                 for (int j = 1; j < needle.length(); j++) {
@@ -10,6 +14,7 @@ class Solution {
                         flag = false;
                     }
                 }
+                // 刚好匹配完结束
                 if (flag == true)
                     return i;
             }
