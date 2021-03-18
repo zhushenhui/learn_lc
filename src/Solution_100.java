f1b6388aaa08d9b16b03ca8ab62c27546c7851f3
zhushenhui <908682858@qq.com>
2021-03-18 22:49:53 +0800

    修改了下

--- src/Solution_100.java
+++ src/Solution_100.java
@@ -21,10 +21,7 @@ public class Solution_100 {
     public static boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
-        } else if (p == null || q == null) {
-            return false;
-        }
-        if (p.val != q.val) {
+        } else if (p == null || q == null || p.val != q.val) {
             return false;
         }
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
