a86433705b6a3c12938414c44376ffc1be423b00
zhushenhui <908682858@qq.com>
2021-03-20 16:17:16 +0800

    //110. 平衡二叉树

---
+++ src/Solution_110.java
@@ -0,0 +1,29 @@
+//110. 平衡二叉树
+public class Solution_110 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+    // 求左子树和右子树的最大高度
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+}
