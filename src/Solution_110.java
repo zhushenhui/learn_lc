521993e61617453fd4c1639f214131d4c3245897
zhushenhui <908682858@qq.com>
2021-03-20 17:01:51 +0800

    110修改

--- src/Solution_110.java
+++ src/Solution_110.java
@@ -16,7 +16,8 @@ public class Solution_110 {
         if (root == null) {
             return true;
         } else {
-            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+            if(Math.abs(height(root.left) - height(root.right)) > 1) return false;
+            return isBalanced(root.left) && isBalanced(root.right);
         }
     }
     // 求左子树和右子树的最大高度
@@ -25,5 +26,16 @@ public class Solution_110 {
             return 0;
         }
         return Math.max(height(root.left), height(root.right)) + 1;
+    }
+
+    private int recur(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = recur(root.left);
+        if (left == -1) return -1;
+        int right = recur(root.right);
+        if (right == -1) return -1;
+        return Math.abs(left - right) < 2 ? Math.max(left, right) : -1;
     }
 }
